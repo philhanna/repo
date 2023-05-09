c8dc96cc3ee3049e1528e4cc6444c21ae6a62fe0
Phil Hanna <ph1204@gmail.com>
2023-05-09 10:45:41 -0400

    Finished

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/pkg/browser"
 	"log"
 	"os"
 	"strings"
 
 	git "github.com/go-git/go-git/v5"
+)
+
+const (
+	GIT_SUFFIX    = ".git"
+	GITHUB_PREFIX = "git@github.com:"
+	HTTP_PREFIX   = "http:"
+	HTTPS_PREFIX  = "https:"
+	MY_PREFIX     = "https://github.com"
 )
 
 // ---------------------------------------------------------------------
@@ -16,9 +25,8 @@ import (
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, `usage: issues [-h] <path>
-
-issues: Launches a browser window with the "issues" page of the specified repository.
+		fmt.Fprintf(os.Stderr, `usage: issues [OPTIONS] [PATH]
+Launches a browser window with the "issues" page of the specified repository.
 
 positional arguments:
   path           local path to repository (default: ".")
@@ -30,61 +38,49 @@ options:
 }
 
 func main() {
-	const (
-		GIT_SUFFIX    = ".git"
-		GITHUB_PREFIX = "git@github.com:"
-		MY_PREFIX     = "https://github.com"
-	)
 
+	// Get command line arguments
 	flag.Parse()
+	path := flag.Arg(0)
 
-	url, err := GetRemoteURL(".")
+	// Get the repository at that path
+	repo, err := git.PlainOpen(path)
 	if err != nil {
-		log.Fatalf("Could not get remote URL: %v", err)
+		log.Fatal(err)
 	}
 
+	// From the repository, get the "origin" remote
+	remote, err := repo.Remote("origin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// From the remote, get the first configured URL
+	url := remote.Config().URLs[0]
+
+	// Trim any ".git" suffix
 	if strings.HasSuffix(url, GIT_SUFFIX) {
 		url = strings.TrimSuffix(url, ".git")
 	}
-	if strings.HasPrefix(url, GITHUB_PREFIX) {
-		url = strings.TrimPrefix(url, GITHUB_PREFIX)
-		url = strings.Join([]string{MY_PREFIX, url}, "/")
-	}
-	fmt.Printf("%v\n", url)
 
-	// browser.OpenURL(issuesURL)
+	// Handle this URL according to its type:
+	switch {
+	case strings.HasPrefix(url, GITHUB_PREFIX):
+		url = GetURLFromGitURL(url)
+	case strings.HasPrefix(url, HTTP_PREFIX):
+	case strings.HasPrefix(url, HTTPS_PREFIX):
+	default:
+		log.Fatalf("Unsupported url type: %s\n", url)
+	}
+
+	issuesURL := strings.Join([]string{url, "issues"}, "/")
+
+	browser.OpenURL(issuesURL)
 }
 
-func GetRemoteURL(path string) (string, error) {
-
-	repo, err := GetRepository(path)
-	if err != nil {
-		return "", err
-	}
-
-	// From the repository, get the origin remote
-	remote, err := repo.Remote("origin")
-	if err != nil {
-		return "", err
-	}
-
-	// Return the first URL
-	url := remote.Config().URLs[0]
-
-	return url, nil
-}
-
-// GetRepository returns a pointer to the specified repository
-func GetRepository(path string) (*git.Repository, error) {
-	repo, err := git.PlainOpen(path)
-	if err != nil {
-		return nil, err
-	}
-	return repo, nil
-}
-
-
-func GetRemote(repo *git.Repository) (*git.Remote, error) {
-	remote, err := repo.Remote("origin")
-	return remote, err
+// GetURLFromGitURL changes a git@github.com: prefix to https://github.com
+func GetURLFromGitURL(url string) string {
+	url = strings.TrimPrefix(url, GITHUB_PREFIX)
+	url = strings.Join([]string{MY_PREFIX, url}, "/")
+	return url
 }
