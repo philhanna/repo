96e496974f1f1d12e718153291669786eca0f120
Phil Hanna <ph1204@gmail.com>
2023-06-02 06:11:18 -0400

    Added unit tests for parseIssueNumber

---
+++ issues_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_parseIssueNumber(t *testing.T) {
+	tests := []struct {
+		name string
+		s string
+		want int
+	}{
+		{"Empty string", "", NO_ISSUE},
+		{"No digits string", "bogus", NO_ISSUE},
+		{"Simple integer", "3", 3},
+		{"Multidigit integer", "35", 35},
+		{"With octothorpe", "#35", 35},
+		{"With prefix", "issue#17", 17},
+		{"In branch name", "defect#35-rename", 35},
+		{"With multiple groups of numbers", "1 2 3", 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			have := parseIssueNumber(tt.s)
+			assert.Equal(t, want, have)
+		})
+	}
+}
