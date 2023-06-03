890651cb0c1e486ca18b346f2a762bd3c83d24ad
Phil Hanna <ph1204@gmail.com>
2023-06-02 22:18:07 -0400

    Figured out how to get the current branch name

--- repo_test.go
+++ repo_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	git "gopkg.in/src-d/go-git.v4"
 )
 
 func Test_parseIssueNumber(t *testing.T) {
@@ -28,4 +31,31 @@ func Test_parseIssueNumber(t *testing.T) {
 			assert.Equal(t, want, have)
 		})
 	}
+}
+
+func TestRepo_GetBranchName(t *testing.T) {
+	branchName := GetBranchName()
+	fmt.Printf("DEBUG: branch name is %q\n", branchName)
+}
+
+func GetBranchName() string {
+
+	// Open the git repository
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		log.Println("Could not open repository")
+		log.Fatal(err)
+	}
+	
+	// Retrieve the current branch reference:
+	ref, err := repo.Head()
+	if err != nil {
+		log.Println("Could not get the HEAD reference")
+		log.Fatal(err)
+	}
+
+	// Extract the branch name from the reference
+	branchName := ref.Name().Short()
+
+	return branchName
 }
