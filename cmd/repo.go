3fc456c731f76d62a7db761bc5f9160c8f784842
Phil Hanna <ph1204@gmail.com>
2023-07-05 09:38:10 -0400

    Finished new mainline

--- cmd/repo.go
+++ cmd/repo.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/philhanna/repo"
+	"github.com/pkg/browser"
+)
+
+// ---------------------------------------------------------------------
+// Functions
+// ---------------------------------------------------------------------
+
+func main() {
+	url := repo.GetURL()
+	browser.OpenURL(url)
+}
