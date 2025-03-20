cab14e6a0c9daf261f4d3620ba1a75dd4ada9bb3
Phil Hanna <ph1204@gmail.com>
2025-03-20 00:15:49 -0400

    Suppress warning errors with 2>/dev/null

--- cmd/repo.go
+++ cmd/repo.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/philhanna/repo"
 	"github.com/pkg/browser"
 )
@@ -10,6 +13,20 @@ import (
 // ---------------------------------------------------------------------
 
 func main() {
+
+	// Get the url of the repository
 	url := repo.GetURL()
+
+	// Ignore bogus warning messages written to stderr
+	nullFile, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening /dev/null:", err)
+		return
+	}
+	defer nullFile.Close()
+
+	os.Stderr = nullFile
+
+	// Open the browser window
 	browser.OpenURL(url)
 }
